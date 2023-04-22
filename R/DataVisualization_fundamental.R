1467f767205fd0d3e7c40145b930dd34963a36a1
axmed_maxamuud <axmedmaxamuud@yahoo.com>
2023-04-22 23:08:33 +0300

    uploading data viz

--- R/DataVisualization_fundamental.R
+++ R/DataVisualization_fundamental.R
@@ -5,21 +5,51 @@ today <- Sys.Date()
 # load packages
 library(tidyverse)
 library(ggplot2)
+library(scales)
 library(readxl)
 library(openxlsx)
 
 # load data
-data <- read_excel("input/data/survey_data.XLSX")
+data <- read_excel("input/data/survey_data.XLSX") %>% 
+  mutate(ki_age = case_when(
+    age >= 18 & age <= 59 ~ "18 - 59 Years",
+    age > 59 ~ "60+ Years"
+  )) %>% group_by(gender) %>% 
+  summarise(total = n()) %>% 
+  mutate(gender_pct = round(total / 500 * 100, digits = 2))
 
-# First chart (Bar chart with proportions % for two variables)
+# define color codes
+wfp_blue <- "#0A6EB4"
+wfp_aque <- "#26bde2"
+
+# creating bar chart
 data %>% 
   ggplot()+
-  aes(gender, fill = hh_head)+
-  geom_bar(position = "dodge")+
-  # add title and fix labs
-  labs(x = "", y = "proportion (%)", title = "Household Head by Gender",
-       subtitle = "% of household heads by their gender", caption = "©Organization Name, Data")+
-  # adding theme
-  theme_minimal()+
+  # define aesthetics
+  aes(x = gender, y = gender_pct, fill = gender)+
+  # define geometries 
+  geom_col()+
+  geom_text(aes(label = gender_pct), hjust = 0.1)+
+  # applying brand colors
+  scale_fill_manual(values = c(female = wfp_aque, male = wfp_blue))+
+  # adding title and subtitles
+  labs(title = "Household head by Gender", 
+       subtitle = "% of household heads by their gender", 
+       caption = "©WFP Somalia, Outcome Monitoring 2023", fill = "Gender")+
+  # apply theme
+  theme_void()+
   theme(legend.position = "top")
+  
+# save chart in png and pdf formats
+ggsave(filename = "output/charts/gender_bar_chart.png",
+       height = 8,
+       width = 11,
+       units = "in",
+       dpi = 300)
+
+# save the chart in pdf format
+ggsave(filename = "output/charts/gender_bar_chart.pdf",
+       height = 8,
+       width = 11)
+
 
